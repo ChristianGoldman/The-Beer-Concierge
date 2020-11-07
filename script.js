3406140828c646d6ac174f3218c9522d4dd2c300
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-11-07 11:51:20 -0600

    Added function to get a hotel name list from tripAdvisor API

--- script.js
+++ script.js
@@ -2,31 +2,59 @@ $(".btn").on("click", function() {
     let location = $("#searchlocation").val();
     console.log(location);
 
+    getHotelInfo(location);
+
+
+
+    let beerURL = "https://api.openbrewerydb.org/breweries?by_city=" + location;
+
+    $.ajax({
+        url: beerURL,
+        method: "GET",
+    }).then(function(response) {
+        console.log(response);
+    })
+
+
+});
+
+function getHotelInfo(city) {
+
     const settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://tripadvisor-com.p.rapidapi.com/hotel/search?location=" + location + "=1&offset=0&language=en&currency=USD",
+        "url": "https://tripadvisor-com.p.rapidapi.com/hotel/search?location=" + city + "=1&offset=0&language=en&currency=USD",
         "method": "GET",
         "headers": {
-            "x-rapidapi-key": "66caed8e94mshaafad0b4be0f6f7p179bc0jsna15f212d5e21",
+            "x-rapidapi-key": "e46221a643mshaa778c220df7fe7p12e0e2jsnc4033c1b7809",
             "x-rapidapi-host": "tripadvisor-com.p.rapidapi.com"
         }
     };
     
     $.ajax(settings).done(function (response) {
         console.log(response);
+
+        let getHotel = response.data;
+
+        let hotelList= [];
+
+        
+        for (let i=0; i < getHotel.length ; i++) {
+
+            let getHotelName = response.data[i].name;
+            hotelList.push(getHotelName);
+
+            let hotelName = $("<div>");
+
+            let hotelNameEl = $("<a>");
+            hotelNameEl.attr("href", response.data[i].website);
+            hotelNameEl.attr("target", "_blank");
+            hotelNameEl.text(hotelList[i]);
+
+            hotelName.append(hotelNameEl);
+            console.log(hotelList);
+
+            $("#hotelResults").append(hotelName);
+        };
     });
-
-
-
-let beerURL = "https://api.openbrewerydb.org/breweries?by_city=" + location;
-
-$.ajax({
-    url: beerURL,
-    method: "GET",
-}).then(function(response) {
-    console.log(response);
-})
-
-
-})
+};
